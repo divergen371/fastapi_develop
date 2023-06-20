5359ba1a1af0a9e727887f52eb2afa7118293364
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-20 10:45:24 +0000

    :sparkles: リクエストとレスポンスの定義

--- api/routers/done.py
+++ api/routers/done.py
@@ -3,11 +3,11 @@ from fastapi import APIRouter
 router = APIRouter()
 
 
-@router.put(path="/tasks/{task_id}/done")
-async def mark_task_as_done():
-    pass
+@router.put(path="/tasks/{task_id}/done", response_model=None)
+async def mark_task_as_done(task_id: int):
+    return
 
 
-@router.delete(path="/tasks/{task_id}/done")
-async def unmark_task_as_done():
-    pass
+@router.delete(path="/tasks/{task_id}/done", response_model=None)
+async def unmark_task_as_done(task_id: int):
+    return
--- api/routers/task.py
+++ api/routers/task.py
@@ -12,15 +12,15 @@ async def list_tasks():
 
 
 @router.post(path="", response_model=task_schema.TaskCreateResponse)
-async def create_task(task_body: task_schema.TaskCreate): 
-    return task_schema.TaskCreateResponse(id=1,**task_body.dict())
+async def create_task(task_body: task_schema.TaskCreate):
+    return task_schema.TaskCreateResponse(id=1, **task_body.dict())
 
 
-@router.put(path="{task_id}")
-async def update_task():
-    pass
+@router.put(path="/{task_id}", response_model=task_schema.TaskCreateResponse)
+async def update_task(task_id: int, task_body: task_schema.TaskCreate):
+    return task_schema.TaskCreateResponse(id=task_id, **task_body.dict())
 
 
-@router.delete(path="{task_id}")
-async def task_delete():
-    pass
+@router.delete(path="/{task_id}", response_model=None)
+async def task_delete(task_id: int):
+    return
