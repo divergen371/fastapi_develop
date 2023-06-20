5939cee32219b2886e355ff6ae54aa333b2bd3b3
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-20 09:23:21 +0000

    :alembic: スキーマの定義

--- api/schemas/task.py
+++ api/schemas/task.py
@@ -1,7 +1,24 @@
 from pydantic import BaseModel, Field
 
 
-class Task(BaseModel):
-    id: int
-    title: str | None = Field(None, example="クリーニングを取りに行く")
+class TaskBase(BaseModel):
+    title: str | None = Field(default=None, example="クリーニングを取りに行く")
+
+
+class TaskCreate(TaskBase):
+    pass
+
+
+class TaskCreateResponse(TaskCreate):
+    task_create_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class Task(TaskBase):
+    task_id: int
     done: bool = Field(False, description="完了フラグ")
+
+    class Config:
+        orm_mode = True
