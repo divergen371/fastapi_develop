fc6fdd05df6df0d0ce8eedddcdc9906499bf708c
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-20 09:14:10 +0000

    :alembic: レスポンス型を定義してAPIに返却させてみる

--- api/routers/task.py
+++ api/routers/task.py
@@ -1,12 +1,14 @@
 # Third Party Library
 from fastapi import APIRouter
 
+import api.schemas.task as task_schema
+
 router = APIRouter(prefix="/tasks")
 
 
-@router.get(path="")
+@router.get(path="", response_model=list[task_schema.Task])
 async def list_tasks():
-    pass
+    return [task_schema.Task(id=1, title="1つ目のToDoタスク")]
 
 
 @router.post(path="")
