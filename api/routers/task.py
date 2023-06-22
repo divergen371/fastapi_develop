7b3371149d0648342474918fc2c59e4fd0f501bf
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-22 12:21:24 +0000

    :sparkles: CRUDのUPDATEを実装/タスクの内容を更新

--- api/routers/task.py
+++ api/routers/task.py
@@ -1,5 +1,5 @@
 # Third Party Library
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 import api.schemas.task as task_schema
@@ -11,19 +11,28 @@ router = APIRouter(prefix="/tasks")
 
 @router.get(path="", response_model=list[task_schema.Task])
 async def list_tasks(db: Session = Depends(get_db)):
-    return task_crud.get_tasks_with_done(db)
+    return task_crud.get_tasks_with_done(db=db)
 
 
 @router.post(path="", response_model=task_schema.TaskCreateResponse)
 async def create_task(
     task_body: task_schema.TaskCreate, db: Session = Depends(get_db)
 ):
-    return task_crud.create_task(db, task_body)
+    return task_crud.create_task(db=db, task_create=task_body)
 
 
 @router.put(path="/{task_id}", response_model=task_schema.TaskCreateResponse)
-async def update_task(task_id: int, task_body: task_schema.TaskCreate):
-    return task_schema.TaskCreateResponse(id=task_id, **task_body.dict())
+async def update_task(
+    task_id: int,
+    task_body: task_schema.TaskCreate,
+    db: Session = Depends(get_db),
+):
+    task = task_crud.get_task(db=db, task_id=task_id)
+    if task is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Task not found."
+        )
+    return task_crud.update_task(db=db, task_create=task_body, original=task)
 
 
 @router.delete(path="/{task_id}", response_model=None)
