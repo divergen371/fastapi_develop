19510035b6257f633974edb291b42ec122596944
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-06-23 11:31:10 +0900

    :rocket: CRUDを非同期処理に対応させた

--- api/routers/done.py
+++ api/routers/done.py
@@ -1,32 +1,35 @@
-from fastapi import APIRouter, HTTPException, Depends, status
+# Third Party Library
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from sqlalchemy.orm import Session
-import api.schemas.done as done_schema
+# First Party Library
 import api.cruds.done as done_crud
-
+import api.schemas.done as done_schema
 from api.db import get_db
 
-router = APIRouter(tags={"done"})
+router = APIRouter(tags=["done"])
 
 
 @router.put(
     path="/tasks/{task_id}/done", response_model=done_schema.DoneResponse
 )
-async def mark_task_as_done(task_id: int, db: Session = Depends(get_db)):
-    done = done_crud.get_done(db=db, task_id=task_id)
+async def mark_task_as_done(task_id: int, db: AsyncSession = Depends(get_db)):
+    done = await done_crud.get_done(db=db, task_id=task_id)
     if done is not None:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Done already exists.",
         )
-    return done_crud.create_done(db=db, task_id=task_id)
+    return await done_crud.create_done(db=db, task_id=task_id)
 
 
 @router.delete(path="/tasks/{task_id}/done", response_model=None)
-async def unmark_task_as_done(task_id: int, db: Session = Depends(get_db)):
-    done = done_crud.get_done(db=db, task_id=task_id)
+async def unmark_task_as_done(
+    task_id: int, db: AsyncSession = Depends(get_db)
+):
+    done = await done_crud.get_done(db=db, task_id=task_id)
     if done is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Done not found."
         )
-    return done_crud.delete_done(db=db, original=done)
+    return await done_crud.delete_done(db=db, original=done)
--- api/routers/task.py
+++ api/routers/task.py
@@ -1,45 +1,48 @@
 # Third Party Library
 from fastapi import APIRouter, Depends, HTTPException, status
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
-import api.schemas.task as task_schema
+# First Party Library
 import api.cruds.tasks as task_crud
+import api.schemas.task as task_schema
 from api.db import get_db
 
 router = APIRouter(prefix="/tasks", tags=["tasks"])
 
 
 @router.get(path="", response_model=list[task_schema.Task])
-async def list_tasks(db: Session = Depends(get_db)):
-    return task_crud.get_tasks_with_done(db=db)
+async def list_tasks(db: AsyncSession = Depends(get_db)):
+    return await task_crud.get_tasks_with_done(db=db)
 
 
 @router.post(path="", response_model=task_schema.TaskCreateResponse)
 async def create_task(
-    task_body: task_schema.TaskCreate, db: Session = Depends(get_db)
+    task_body: task_schema.TaskCreate, db: AsyncSession = Depends(get_db)
 ):
-    return task_crud.create_task(db=db, task_create=task_body)
+    return await task_crud.create_task(db=db, task_create=task_body)
 
 
 @router.put(path="/{task_id}", response_model=task_schema.TaskCreateResponse)
 async def update_task(
     task_id: int,
     task_body: task_schema.TaskCreate,
-    db: Session = Depends(get_db),
+    db: AsyncSession = Depends(get_db),
 ):
-    task = task_crud.get_task(db=db, task_id=task_id)
+    task = await task_crud.get_task(db=db, task_id=task_id)
     if task is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Task not found."
         )
-    return task_crud.update_task(db=db, task_create=task_body, original=task)
+    return await task_crud.update_task(
+        db=db, task_create=task_body, original=task
+    )
 
 
 @router.delete(path="/{task_id}", response_model=None)
-async def task_delete(task_id: int, db: Session = Depends(get_db)):
-    task = task_crud.get_task(db=db, task_id=task_id)
+async def task_delete(task_id: int, db: AsyncSession = Depends(get_db)):
+    task = await task_crud.get_task(db=db, task_id=task_id)
     if task is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Task not found"
         )
-    return task_crud.delete_task(db, original=task)
+    return await task_crud.delete_task(db, original=task)
